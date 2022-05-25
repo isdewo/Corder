cc459c16c3faedf9e1a9511b0191c54b5dd319a3
Jaewoo Choi <chlwodn980921@gmail.com>
2022-05-26 03:39:14 +0900

    회원가입, 로그인 1차 구현

--- app/src/main/java/com/example/corder/JoinActivity.kt
+++ app/src/main/java/com/example/corder/JoinActivity.kt
@@ -1,12 +1,99 @@
 package com.example.corder
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.corder.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import kotlin.random.Random
 
 class JoinActivity : AppCompatActivity(){
+
+    private lateinit var auth: FirebaseAuth
+    private val database = Firebase.database.reference
+    private lateinit var curUser: FirebaseUser
+
+    private var userSort = 0
+    private var isBlank = false // 빈칸 확인
+
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
+
+        auth = FirebaseAuth.getInstance()
+
+        val idCreation = findViewById<Button>(R.id.idCreation)
+        idCreation.setOnClickListener{
+            register()
+        }
+
+    }
+
+    private fun register(){
+        val inputId = findViewById<EditText>(R.id.inputId)
+        val email = inputId.text.toString().trim()
+        val inputPw = findViewById<EditText>(R.id.inputPw)
+        val password = inputPw.text.toString().trim()
+        val inputName = findViewById<EditText>(R.id.inputName)
+        val name = inputName.text.toString().trim()
+
+        val sellerButton = findViewById<Button>(R.id.button3)
+        sellerButton.setOnClickListener{
+            val userNumb = Random.nextInt(0,200)
+            userSort = userNumb
+        }
+
+        val customerButton = findViewById<Button>(R.id.button2)
+        customerButton.setOnClickListener {
+            userSort = 0
+        }
+
+        if (email.isEmpty() || password.isEmpty() || name.isEmpty()) {
+            isBlank = true
+            return
+        }
+        else{
+            isBlank = false
+        }
+
+        if (!isBlank) {
+            auth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this){ task ->
+                    if(task.isSuccessful){
+                        Log.d(TAG,"회원가입 성공")
+                        val user = auth.currentUser
+                        Toast.makeText(this, "회원가입 성공", Toast.LENGTH_SHORT).show()
+                        updateUI(user)
+                    }
+                    else{
+                        Log.d(TAG,"회원가입 실패",task.exception)
+                        Toast.makeText(this, "회원가입 실패", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+        else {
+            if (isBlank) {   // 작성 안한 항목이 있을 경우
+                Toast.makeText(this, "빈 칸을 모두 채워 주세요",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    private fun updateUI(user: FirebaseUser?){
+        if(user != null){
+            // 로그인 화면으로 이동
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            startActivity(intent)
+            curUser = Firebase.auth.currentUser!!
+        }
     }
 }
--- app/src/main/java/com/example/corder/LoginActivity.kt
+++ app/src/main/java/com/example/corder/LoginActivity.kt
@@ -1,20 +1,81 @@
 package com.example.corder
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val goJoinbtn = findViewById<Button>(R.id.goJoin)
+        auth = Firebase.auth
 
-        goJoinbtn.setOnClickListener {
-            // 자리현황 확인/변경
+        val joinButton = findViewById<Button>(R.id.goJoin)
+        joinButton.setOnClickListener {
+            // 회원가입
             startActivity(Intent(this, JoinActivity::class.java))
+        }
+
+        val loginButton= findViewById<Button>(R.id.gotoLogin)
+        loginButton.setOnClickListener {
+            login()
+        }
+    }
+
+    private fun login(){
+        val em = findViewById<EditText>(R.id.editTextTextPersonName4)
+        val email = em.text.toString()
+        val pw = findViewById<EditText>(R.id.editTextTextPersonName5)
+        val password = pw.text.toString()
+
+        if(email.isBlank()|| password.isBlank()){
+            Toast.makeText(this, "빈 칸을 모두 채워 주세요", Toast.LENGTH_SHORT).show()
+            return
+        }
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this){ task->
+                if(task.isSuccessful){
+                    //로그인 성공
+                    val user = auth.currentUser
+                    Toast.makeText(this, "환영합니다!",Toast.LENGTH_SHORT).show()
+                    loginSave(email,password)
+                    updateUI(user)
+                }
+                else{
+                    //로그인 실패
+                    Toast.makeText(this, "로그인 실패",Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+
+    }
+
+    private fun loginSave(a:String,b:String){
+        val pref = getSharedPreferences("preferences", MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = pref.edit()
+        editor.putString("email", a)
+        editor.putString("password",b)
+        editor.putBoolean("login", true)
+        editor.commit()
+    }
+    private fun updateUI(user: FirebaseUser? ){
+        if(user != null) {
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
         }
 
     }
