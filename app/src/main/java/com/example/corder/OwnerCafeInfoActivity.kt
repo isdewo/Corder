185b565aa6e073b6bb9d00cc2147c2ba8207e7c2
Jaewoo Choi <chlwodn980921@gmail.com>
2022-06-12 05:17:47 +0900

    add cafe image to firebase storage

--- app/src/main/java/com/example/corder/OwnerCafeInfoActivity.kt
+++ app/src/main/java/com/example/corder/OwnerCafeInfoActivity.kt
@@ -1,64 +1,123 @@
 package com.example.corder
 
+import android.content.ContentValues
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.icu.text.SimpleDateFormat
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.activity_owner_cafe_info.*
+import java.util.*
+
 
 class OwnerCafeInfoActivity : AppCompatActivity() {
 
-    private lateinit var database : DatabaseReference
+    private lateinit var database: DatabaseReference
+    private lateinit var storageRef: StorageReference
+    private lateinit var storage: FirebaseStorage
+    private val resultLauncher: ActivityResultLauncher<Intent>? = null
+    var uri: Uri? = null
+    var imgUri: String = ""
+    private val PICK_IMAGE = 10001
+    val calendar:Calendar = Calendar.getInstance()
+    private val getContent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+        uri= (result.data?.data)
+        uri?.let { upload(it) }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_owner_cafe_info)
 
+
         val user = Firebase.auth.currentUser
         var uid = "0"
-        if(user != null){
+        if (user != null) {
             Toast.makeText(this, "성공", Toast.LENGTH_SHORT).show()
             uid = user.uid
-        }else{
+        } else {
             Toast.makeText(this, "실패", Toast.LENGTH_SHORT).show()
         }
 
         cafeCode.text = uid
+
+        storage = FirebaseStorage.getInstance()
+        val getImg = findViewById<Button>(R.id.getImgBtn)
+        getImg.setOnClickListener {
+            val intent: Intent = Intent(Intent.ACTION_GET_CONTENT).apply { type = "image/*" }
+           getContent.launch(intent)
+            uri?.let { upload(it) }
+        }
+
         val clickSavebtn = findViewById<Button>(R.id.saveBtn)
         clickSavebtn.setOnClickListener {
-//            var num = cafeNum.text.toString()
 
-            val imgUrl = "http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcS9DvbOUdtK8lQS6VYAfWQokJcjDSJqL2gzA7oKVIfBJfrRuHSA7p85-bbuaLU0vlKAK-zvPmgK9EAGbYE8ao8"
+//            Toast.makeText(this, "저장버튼: ${imgUri}", Toast.LENGTH_SHORT).show()
+            imgUri = uri.toString()
             val name = cafeName.text.toString()
             val owner = ownerName.text.toString()
             val address = cafeAddress.text.toString()
             val call = cafeCall.text.toString()
             val time = cafeTime.text.toString()
             val limit = limitPrice.text.toString()
-            if(name != "" && owner != "" && address != "" && call != "" && time != "" && uid != "0") {
+            if (imgUri != "" && name != "" && owner != "" && address != "" && call != "" && time != "" && uid != "0") {
                 database = Firebase.database.reference
-                val listData =  ListData(imgUrl, uid, name, owner, address, call, time, limit.toInt())
+//                Toast.makeText(this, "${imgUri}", Toast.LENGTH_SHORT).show()
+                val listData =
+                    ListData(imgUri, uid, name, owner, address, call, time, limit.toInt())
                 database.child("cafes").child(uid).setValue(listData)
-//                database.child("cafes").child(uid).child("imgUrl").setValue("http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcS9DvbOUdtK8lQS6VYAfWQokJcjDSJqL2gzA7oKVIfBJfrRuHSA7p85-bbuaLU0vlKAK-zvPmgK9EAGbYE8ao8")
-//                database.child("cafes").child(uid).child("cafeCode").setValue(uid)
-//                database.child("cafes").child(uid).child("cafeName").setValue(name)
-//                database.child("cafes").child(uid).child("ownerName").setValue(owner)
-//                database.child("cafes").child(uid).child("cafeAddress").setValue(address)
-//                database.child("cafes").child(uid).child("cafeCall").setValue(call)
-//                database.child("cafes").child(uid).child("cafeTime").setValue(time)
+
 
                 Toast.makeText(this, "카페 정보가 저장되었습니다.", Toast.LENGTH_SHORT).show()
                 var intent = Intent(this, LoginActivity::class.java)
                 startActivity(intent)
-            }else {
+            } else {
                 Toast.makeText(this, "정보저장에 실패했습니다.\n다시 입력해주세요.", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
+
+
+//    private fun funImageUpload(view : View){
+//        var timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+//        var imgFilename = "IMAGE_" + timeStamp + "_.png"
+//        var storageRef = storage?.reference?.child("images")?.child(imgFilename)
+//
+//        storageRef?.putFile(uri!!)?.addOnSuccessListener {
+//            Toast.makeText(view.context, "Image Uploaded", Toast.LENGTH_SHORT).show()
+//        }
+//    }
+
+    private fun upload(uri: Uri) {
+        storageRef = storage.reference
+        val sdf = SimpleDateFormat("yyyyMMddhhmmss")
+        val fileName = sdf.format(calendar.timeInMillis) + ".jpg"
+        val riversRef = storageRef.child("/$fileName")
+
+        riversRef.putFile(uri)
+            .addOnCompleteListener(this) { taskSnapshot ->
+                if (taskSnapshot.isSuccessful) {
+                    Log.d(ContentValues.TAG, "업로드 성공")
+                    Toast.makeText(this, "업로드 성공", Toast.LENGTH_SHORT).show()
+                } else {
+                    Log.d(ContentValues.TAG, "업로드 실패", taskSnapshot.exception)
+                    Toast.makeText(this, "업로드 실패", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+    }
 }
