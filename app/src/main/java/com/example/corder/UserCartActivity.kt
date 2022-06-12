e648b8f42bc7a76bcdb6a82417129a589992d441
sungwon kim <44858739+kimsungwon1@users.noreply.github.com>
2022-06-12 15:51:58 +0900

    kimsungwon push - 6/11

--- app/src/main/java/com/example/corder/UserCartActivity.kt
+++ app/src/main/java/com/example/corder/UserCartActivity.kt
@@ -1,11 +1,18 @@
 package com.example.corder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class UserCartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_cart)
+
+        val menuEat = findViewById<Button>(R.id.menuEat)
+        menuEat.setOnClickListener{
+            startActivity(Intent(this, SeatShowActivity::class.java))
+        }
     }
 }
